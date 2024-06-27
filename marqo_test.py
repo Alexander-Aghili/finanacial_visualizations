b9687c0b402c65f7141d4122b8ca22b8869c8567
Alexander Aghili <awaghili@ucsc.edu>
2024-06-27 13:17:27 -0700

    New data idea

--- marqo_test.py
+++ marqo_test.py
@@ -1,41 +1,45 @@
 import marqo
-import glob
+import json
 import pprint
 
 mq = marqo.Client(url='http://localhost:8882')
-"""
-mq.index("images-index").delete()
-mq.create_index(
-        "images-index",
-        treat_urls_and_pointers_as_images=True,
-        model="open_clip/ViT-B-32/laion2b_s34b_b79k",
-)
-"""
-images = []
-for path in glob.glob('images/*png'):
-    images.append({"Title": path.replace(".png","").replace("/", " "), "image": "http://host.docker.internal:8222/" + path})
+
+
+def create_base_index():
+    mq.create_index(
+            "index",
+            model="hf/e5-base-v2"
+    )
+
+def get_mappings(data):
+    json_obj = data[0]
+    # Parse the JSON object if it's a string
+    if isinstance(json_obj, str):
+        json_obj = json.loads(json_obj)
     
-print(images)
+    # Extract all keys from the JSON object
+    keys = list(json_obj.keys())
+    
+    # Calculate the weight for each key
+    num_keys = len(keys)
+    weight = 1.0 / num_keys if num_keys > 0 else 0
+    
+    # Create the weights dictionary
+    weights = {key: weight for key in keys}
+    
+    return {
+            'mapped_data': {
+                'type': "multimodal_combination",
+                "wiehgts": weights,
+            },
+        }
 
+def add_index(data):  
+    mq.index("index").add_documents(
+            documents=data,
+            mappings=get_mappings(data),
+            tensor_fields=['mapped_data']        
+    )
 
-res = mq.index("images-index").add_documents(
-        documents=images,
-        mappings={
-            "images": {
-                "type": "multimodal_combination",
-                "weights": {
-                    "Title": 0.3,
-                    "image": 0.7
-                }
-            }
-        },
-        tensor_fields=["images"]
-)
-
-print(res)
-print(mq.index("images-index").get_stats())
-results = mq.index("images-index").search(
-    q="Google"
-)
-
-pprint.pprint(results)
+if __name__ == "__main__":
+    create_base_index()
